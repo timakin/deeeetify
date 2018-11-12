83b709a726b670c1a02ce899ae52006009de53cb
timakin <timaki.st@gmail.com>
2018-11-12 18:27:49 +0900

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/timakin/deeeetify
+
+require (
+	github.com/disintegration/imaging v1.5.0
+	github.com/urfave/cli v1.20.0
+	golang.org/x/image v0.0.0-20181109232246-249dc8530c0e // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"log"
+	"os"
+
+	"github.com/disintegration/imaging"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Action = func(c *cli.Context) error {
+		imagePath := "fall.jpg"
+
+		if c.NArg() > 0 {
+			imagePath = c.Args().Get(0)
+		}
+
+		src, err := imaging.Open(imagePath)
+		if err != nil {
+			log.Fatalf("failed to open image: %v", err)
+		}
+
+		blurred := imaging.Blur(src, 20)
+		//err = imaging.Save(blurred, "out.jpg")
+		//if err != nil {
+		//	log.Fatalf("failed to save image: %v", err)
+		//}
+
+		// Open a test image.
+		beer, err := imaging.Open("./beer_hand.png")
+		if err != nil {
+			log.Fatalf("failed to open image: %v", err)
+		}
+
+		blrect := blurred.Bounds()
+		brect := beer.Bounds()
+
+		dstRectY := float64(blrect.Dy()) * 0.6
+		scaleY := (dstRectY / float64(brect.Dy()))
+
+		log.Printf("blrect -> %d:%d\n", blrect.Dx(), blrect.Dy())
+		log.Printf("brect -> %d:%d\n", brect.Dx(), brect.Dy())
+
+		scaledBeer := imaging.Resize(beer, 0, int(float64(brect.Dy())*scaleY), imaging.Lanczos)
+		err = imaging.Save(scaledBeer, "scaled_beer.jpg")
+		if err != nil {
+			log.Fatalf("failed to save image: %v", err)
+		}
+
+		rgba := image.NewRGBA(blrect)
+		draw.Draw(rgba, blrect, blurred, image.Point{0, 0}, draw.Src)
+		draw.Draw(rgba, scaledBeer.Bounds(), scaledBeer, image.Point{0, 0}, draw.Over)
+
+		out, err := os.Create("out.jpg")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		var opt jpeg.Options
+		opt.Quality = 100
+
+		jpeg.Encode(out, rgba, &opt)
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
